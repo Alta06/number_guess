75966ca7306063f5eb25dd58a70b8d2b957fcadc
Alta06 <antoine6006@gmail.com>
2023-03-15 14:49:46 +0000

    refactor: refactor-with-functions

--- number_guess.sh
+++ number_guess.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+CREATE_USER() {
+$PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, 0)"
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+}
+
+FIND_USER() {
+  USER=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
+BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
+}
+
+PLAY_GAME() {
 SECRET_NUMBER=$(( (RANDOM % 1000) + 1 ))
 NUMBER_OF_GUESSES=1
 
 echo -e "\nEnter your username:"
 read USERNAME
 
-USER=$($PSQL "SELECT name FROM users WHERE name='$USERNAME'")
-GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE name='$USERNAME'")
-BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE name='$USERNAME'")
+FIND_USER
 
 #if no user create one
+
 if [[ -z $USER ]]
   then
-  $PSQL "INSERT INTO users(name, games_played, best_game) VALUES('$USERNAME', 0, 0)"
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-
-  else 
-  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  CREATE_USER
+  else
+  echo -e "\nWelcome back, $USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
+echo $SECRET_NUMBER
 echo -e "\nGuess the secret number between 1 and 1000:"
 read GUESS
 
 #add a games_played + 1
-$PSQL "UPDATE users SET games_played = games_played + 1 WHERE name='$USERNAME'"
+$PSQL "UPDATE users SET games_played = games_played + 1 WHERE username='$USERNAME'"
 
 while [[ $GUESS -ne $SECRET_NUMBER ]]
 do
@@ -50,10 +61,10 @@ done
 
 if [[ $BEST_GAME -eq 0 ]] || [[ $BEST_GAME -gt $NUMBER_OF_GUESSES ]]
   then
-  $PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE name='$USERNAME'"
+  $PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'"
 fi
 
 echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+}
 
-
-
+PLAY_GAME
